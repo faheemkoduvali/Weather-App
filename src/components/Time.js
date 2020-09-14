3767ca04c2c849aac18859fd9186b1cc810f2fdb
faheemkoduvali <52704760+faheemkoduvali@users.noreply.github.com>
2020-09-14 09:32:38 +0530

    2

--- src/components/Time.js
+++ src/components/Time.js
@@ -10,16 +10,15 @@ function Time({off_time}) {
     //     setTime(d)
     // },[time])
     // }
-    var time_int
     useEffect(() => {
-         time_int = setInterval(() => {
+         setInterval(() => {
             let d = new Date();
             d.setUTCSeconds((-330*60)+off_time+d.getUTCSeconds());
             d=d.toTimeString().split(" ")[0]
             setTime(d)
-        })   
+        },Time)   
         return () => {
-            clearInterval(time_int)
+            // clearInterval(time_int)
         }
       })
     return (<div className="Time">{time}</div>)
